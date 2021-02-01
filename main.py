7a7ceef704b84cdbdccedd2da509e14c8a454bac
g-armand <78274164+g-armand@users.noreply.github.com>
2021-02-01 22:12:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import functools                            #functools.partial in order to create unique functions and objects, way to avoid specific issues in the program
 import tkinter as tk                        #GUI module
+import time
+import threading
 
 class Window(tk.Tk):
     def __init__(self):
@@ -7,10 +9,14 @@ class Window(tk.Tk):
 
         self.title('Pw Manager')
         self.geometry('1000x500')
-        self.config(background = '#000000')
+        self.config(background = '#f2f2f2')
 
         self.start = True
         self.position = 0
+        self.txt = "Le Conseil a exprimé le souhait."
+        self.string = self.txt.split(' ')
+        self.count = 0
+
         #go to start menu
         self.startmenu()
 
@@ -27,50 +33,74 @@ class Window(tk.Tk):
             self.mainborder.destroy()
 
         #creation of the blank frame
-        self.mainborder = tk.Frame(self, height = 405, width = 805, bg = '#00ae0c', bd = 3, highlightcolor = "#adcbef")
+        self.mainborder = tk.Frame(self, height = 405, width = 805, bg = '#a97acc', bd = 3, highlightcolor = "#adcbef")
         self.mainborder.pack(expand = 'True')
-        self.mainframe = tk.Frame(self.mainborder, height = 400, width = 800, bg = '#000000', bd = 3, highlightcolor = "#adcbef")
+        self.mainframe = tk.Frame(self.mainborder, height = 400, width = 800, bg = '#f2f2f2', bd = 3, highlightcolor = "#adcbef")
         self.mainframe.pack(expand = 'True', fill = "both")
 
     def startmenu(self):
         self.newframe()
 
-        gamelabel = tk.Label(self.mainframe, width = 80, height = 10 , bg = '#000000', text = "TYPING RACE!!!", font = (20), fg = "#00ae0c")
+        gamelabel = tk.Label(self.mainframe, width = 80, height = 10 , bg = '#f2f2f2', text = "TYPING RACE!!!", font = (20), fg = "#a97acc")
         gamelabel.grid(row = 0)
 
-        startbutton = tk.Button(self.mainframe, width = 15, height = 5, bg = '#00ae0c', command = self.gamemenu)
+        startbutton = tk.Button(self.mainframe, width = 15, height = 5, bg = '#a97acc', command = self.gamemenu)
         startbutton.grid(row = 1)
 
     def gamemenu(self):
+
+        self.timertext = tk.StringVar(self, "0:00")
+        self.timerlabel = tk.Label(self, textvariable = self.timertext, font = 20, bg ="#3EA64C")
+        self.timerlabel.pack(pady = 50, padx = 50, anchor = "ne")
         self.newframe()
 
-        txt = "Le Conseil a exprimé le souhait que toutes les parties soutiennent l'accord en dix points pour la reprise des pourparlers intercommunautaires."
-
-        textmessage = tk.Message(self.mainframe, width = 800, bg = '#000000', text = txt, font = (20), fg = "#00ae0c")
+        textmessage = tk.Message(self.mainframe, width = 800, bg = '#f2f2f2', text = self.txt, font = (20), fg = "#a97acc")
         textmessage.grid(row = 0)
 
-        self.wordlabel = tk.Label(self.mainframe, width = 80, bg = '#000000', text = "word", fg = "#00ae0c")
+        self.word = tk.StringVar(self, self.string[self.count])
+        self.wordlabel = tk.Label(self.mainframe, width = 80, bg = '#f2f2f2', textvariable = self.word, fg = "#a97acc")
         self.wordlabel.grid(row = 1, pady = 20)
 
-        assertcommand = (self.register(self.assertion), "%s", "%S")
+        self.assertcommand = (self.register(self.assertion), "%s", "%S")
 
-        self.wordentry = tk.Entry(self.mainframe, width = 80, bg = '#000000',  fg = "#00ae0c", validate = "key", validatecommand = assertcommand)
+
+        self.wordentry = tk.Entry(self.mainframe, width = 40, bg = '#f2f2f2',  fg = "#a97acc", validate = "key", validatecommand = self.assertcommand)
         self.wordentry.grid(row = 2, pady = 10)
+
+        self.SVl = tk.StringVar(self, self.position)
+        self.l = tk.Label(self.mainframe, textvariable = self.SVl)
+        self.l.grid(row = 1, column = 2, pady = 20)
+
+        self.threadtimer = threading.Thread(self.timer()).start()
+
+    def startgame(self):
+        pass
 
     def assertion(self, typingword, letter):
 
-        word = self.wordlabel.cget("text")
+
+        word = self.string[self.count]
         typedword = typingword + letter
 
+        self.SVl.set(self.position)
+        self.l.update()
 
-        if typedword == word + " " or typedword == word + ",":
+
+        if typedword == word + " " or typedword == (word + ","):
             self.position = 0
-            self.startmenu()
+            self.count += 1
+            self.nextword()
+            return True
+
+        if typedword == self.string[-1]:
+            self.stoptimer()
+            #self.wordentry.configure(validate = "none")
             return True
 
         elif letter == word[self.position]:
             self.position += 1
             return True
+
 
         else:
             return False
@@ -78,14 +108,36 @@ class Window(tk.Tk):
 
 
 
+    def nextword(self):
 
+        self.wordentry.destroy()
+        self.wordentry = tk.Entry(self.mainframe, width = 40, bg = '#f2f2f2',  fg = "#a97acc", validate = "key", validatecommand = self.assertcommand)
+        self.wordentry.grid(row = 2, pady = 10)
+        self.wordentry.focus()
 
+        self.word.set(self.string[self.count])
+        self.wordlabel.update()
 
+    def timer(self):
 
+        timer = time.time()
+        chrono = 0.0
 
+        while True:
+            time.sleep(0.01)
+            a = time.time()
+            chrono = a - timer
+            chrono = str(chrono)
+            self.timertext.set(str(chrono[:4]))
+            chrono = float(chrono)
+            self.timerlabel.update()
 
-
-
+    def stoptimer(self):
+            score = self.timertext.get()
+            self.wordentry.delete(0, "end")
+            self.timerlabel.destroy()
+            self.SVl.set(score)
+            self.l.update()
 
 
 
